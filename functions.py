edd39069d51ed71fbbe761121499fc5be1ca9697
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-03-01 18:47:58 -0600

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,29 @@
+# DRY
+# Excercise!
+def say_hello():
+    print('hellllooooo')
+
+say_hello()
+
+
+picture = [
+    [0,0,0,1,0,0,0],
+    [0,0,1,1,1,0,0],
+    [0,1,1,1,1,1,0],
+    [1,1,1,1,1,1,1],
+    [0,0,0,1,0,0,0],
+    [0,0,0,1,0,0,0]
+]
+
+def show_tree():
+    for row in picture:
+        for pixel in row:
+            if (pixel == 1):
+                print('*', end='')
+            else:
+                print(' ', end='')
+        print('')
+
+show_tree()
+show_tree()
+show_tree()
